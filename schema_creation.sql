2809d558e29373a0b946bc6d779b08f2ecfd77d0
Menachem Geva <me.geva@gmail.com>
2024-10-31 14:55:26 +0200

    Create schema_creation.sql

---
+++ schema_creation.sql
@@ -0,0 +1,36 @@
+create database mahanlake;
+
+CREATE EXTERNAL TABLE IF NOT EXISTS radar_measurements (
+    time DOUBLE,
+    azimuth DOUBLE,
+    elevation DOUBLE,
+    range DOUBLE
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE
+LOCATION 's3://refa-hack1/radar/'
+TBLPROPERTIES (
+    'skip.header.line.count'='1',
+    'serialization.encoding'='UTF-8'
+)CREATE EXTERNAL TABLE IF NOT EXISTS flights (
+    time DOUBLE,
+    x DOUBLE,
+    y DOUBLE,
+    z DOUBLE
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE
+LOCATION 's3://refa-hack1/flights/'
+TBLPROPERTIES (
+    'skip.header.line.count'='1',
+    'serialization.encoding'='UTF-8'
+);
+
+
+
+
+
